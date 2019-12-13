d1ab048724d1464cb41881a9213bd4c9c2cf0b45
Bartosz Trzos <devbartosz@gmail.com>
2019-12-13 10:55:37 +0100

    Remove some unnecessary styles Prettier updates Add my pic 🙈

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,58 +1,57 @@
-<nav>
-<div class="max-w-xl mx-auto flex justify-between">
-	<ul>
-		<li><a class:selected='{segment === undefined}' href='.'>Bartosz Trzos</a></li>
-	</ul>
-	<ul>
-		<li><a class:selected='{segment === "about"}' href='about'>Resume</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li class="self-end"><a rel=prefetch class:selected='{segment === "blog"}' href='blog'>Blog</a></li>
-	</ul>
-</div>
-</nav>
-
 <script>
-	export let segment;
+  export let segment
 </script>
 
 <style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
+  nav {
+    border-bottom: 1px solid rgba(255, 62, 0, 0.1);
+  }
 
-	li {
-		display: block;
-		float: left;
-	}
+  li {
+    display: inline-block;
+  }
 
-	.selected {
-		position: relative;
-		display: inline-block;
-	}
+  .selected {
+    position: relative;
+    display: inline-block;
+  }
 
-	.selected::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
+  .selected::after {
+    position: absolute;
+    content: '';
+    width: calc(100% - 1em);
+    height: 2px;
+    background-color: rgb(255, 62, 0);
+    display: block;
+    bottom: -1px;
+  }
 
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
+  a {
+    text-decoration: none;
+    padding: 1em 0.5em;
+    display: block;
+  }
 </style>
+
+<nav>
+  <div class="max-w-xl mx-auto flex justify-between">
+    <ul>
+      <li>
+        <a class:selected={segment === undefined} href=".">Bartosz Trzos</a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <a class:selected={segment === 'about'} href="about">Resume</a>
+      </li>
+
+      <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+		     the blog data when we hover over the link or tap it on a touchscreen -->
+      <li>
+        <a rel="prefetch" class:selected={segment === 'blog'} href="blog">
+          Blog
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,15 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Bartosz Trzos - JavaScript Engineer</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<div class="flex my-8">
+  <div>
+    <h1>Who am I?</h1>
+    <p>
+      Progressive JavaScript engineer, with passion for UI and UX of web
+      applications. Fast thinker and learner with a deep understanding of
+      frontend side of website creation. Usually, work with Vue and React.
+    </p>
+  </div>
+  <img class="h-64 object-cover rounded-full" src="bartosz.jpg" alt="My face" />
+</div>
